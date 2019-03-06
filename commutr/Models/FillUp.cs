6430e2e15b6878ed486c08fdba085e5e74d481d1
Austin Webre <austin.webre@gmail.com>
2019-03-05 18:49:01 -0600

    bump version to 1.1.6

--- commutr/Models/FillUp.cs
+++ commutr/Models/FillUp.cs
@@ -1,7 +1,6 @@
 using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using commutr.iOS.Annotations;
 using commutr.Services;
 using PropertyChanged;
 using SQLite;
@@ -11,29 +10,29 @@ namespace commutr.Models
     public class FillUp : IIdentifiable, INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        
+
         public DateTime Date { get; set; }
 
         public decimal FuelAmount { get; set; }
-        
+
         public decimal PricePerFuelAmount { get; set; }
 
         [Ignore]
         public decimal Total => Math.Round(FuelAmount * PricePerFuelAmount, 2);
-        
+
         public string Notes { get; set; }
-        
+
         public decimal Distance { get; set; }
-        
+
         [Ignore]
         public decimal FuelEconomy => Math.Round(FuelAmount != 0 ? Distance / FuelAmount : 0, 2);
 
-        [Ignore] 
+        [Ignore]
         public string MPG => $"{FuelEconomy} MPG";
-        
+
         [Indexed]
         public int VehicleId { get; set; }
     }
